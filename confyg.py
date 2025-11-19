e6d9b17f4f369c53f303f2d1fbb308816d12392b
Panda1444 <85384701+Panda1444@users.noreply.github.com>
2025-11-20 00:26:56 +0500

    Create confyg.py

---
+++ confyg.py
@@ -0,0 +1,7 @@
+# ТВОИ НАСТРОЙКИ ЗДЕСЬ
+BOT_TOKEN = "8530151951:AAHZRccqmpGUp_UPTkd9dW21a5VJ4tQ5Dwc"          # ← от @BotFather
+APIROLE_TOKEN = "ar_1a2b3c4d5e6f7g8h9i..." # ← из @APIroleBot
+APIROLE_SHOP_ID = "12345"                 # ← твой ID магазина в APIrole
+ADMIN_ID = 8221378629                     # ← твой Telegram ID (@userinfobot)
+
+# Остальное не трогай
